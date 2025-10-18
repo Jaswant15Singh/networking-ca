f7c5cc51377c63fd5f59f0d947e33284632d3855
Jaswant Singh <singhjaswant0932@gmail.com>
2025-10-18 21:01:52 +0100

    lint,prettier config along with github workflow config

--- networking-backend/index.js
+++ networking-backend/index.js
@@ -1,9 +1,9 @@
-const express=require("express");
-const app=express();
+const express = require('express');
+const app = express();
 
-app.get("/getdata",(req,res)=>{
-    res.status(200).json({message:"Ho gaya"})
-})
-app.listen((5000),()=>{
-    console.log("listening on server");
-})
+app.get('/getdata', (req, res) => {
+  res.status(200).json({ message: 'Ho gaya' });
+});
+app.listen(5000, () => {
+  console.log('listening on server');
+});
