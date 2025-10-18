6047b87baee7e731a48df2ae5c4134ef3d8273cc
Jaswant Singh <singhjaswant0932@gmail.com>
2025-10-18 16:52:53 +0100

    backend docker setup

---
+++ networking-backend/index.js
@@ -0,0 +1,9 @@
+const express=require("express");
+const app=express();
+
+app.get("/getdata",(req,res)=>{
+    res.status(200).json({message:"Ho gaya"})
+})
+app.listen((5000),()=>{
+    console.log("listening on server");
+})
