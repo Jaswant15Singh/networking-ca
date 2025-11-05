c6dbca550a134c1e75665efed4a37526ab6a7153
Jaswant Singh <singhjaswant0932@gmail.com>
2025-11-05 19:55:59 +0000

    testing for home component

--- networking-backend/index.js
+++ networking-backend/index.js
@@ -4,6 +4,10 @@ const app = express();
 app.get('/getdata', (req, res) => {
   res.status(200).json({ message: 'Ho gaya hai bhai' });
 });
+
+app.get('/test', (req, res) => {
+  res.status(200).json({ message: 'for testing purpose' });
+});
 app.listen(5000, () => {
   console.log('listening on server');
 });
