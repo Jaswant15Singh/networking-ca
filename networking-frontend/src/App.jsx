bec3dfd77f7a59c210afa4578940f04fe93172f6
Jaswant Singh <singhjaswant0932@gmail.com>
2025-11-06 21:13:54 +0000

    title name change

--- networking-frontend/src/App.jsx
+++ networking-frontend/src/App.jsx
@@ -1,21 +1,12 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import Home from './Components/Home';
 function App() {
   const [count, setCount] = useState(0);
   return (
     <>
-      <div>
-        <Home />
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <Home />
+
       <h1>Vite + React by </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>Data is {count}</button>
