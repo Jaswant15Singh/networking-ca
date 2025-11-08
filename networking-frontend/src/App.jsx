c1c21df76ebcf70dacdc9896aa934075009d8a44
Jaswant Singh <singhjaswant0932@gmail.com>
2025-11-08 08:44:14 +0000

    pushing on production

--- networking-frontend/src/App.jsx
+++ networking-frontend/src/App.jsx
@@ -10,11 +10,8 @@ function App() {
       <h1>Vite + React by </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>Data is {count}</button>
-        <p>
-          Edited data by Jaswant Singh<code>src/App.jsx</code> and save to test HMR
-        </p>
+        <p>Edited data by Jaswant Abhay Singh for Networking CA.</p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
